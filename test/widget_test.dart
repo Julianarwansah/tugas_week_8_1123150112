1e274ab1dd603a170bb70f4ab7aeb8e96a902d77
NamaAnda <julianarwansahh@gmail.com>
2025-11-21 20:21:42 +0700

    Membuat Material app di dalam main dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tugas_week_8_1123150112/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const JullApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
