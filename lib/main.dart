d46d7b3e06ef40eb83a35fe2f6d0aac5007d7e0d
NamaAnda <julianarwansahh@gmail.com>
2025-11-24 16:53:38 +0700

    menambahkan class dan menyambungkan maindart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_week_8_1123150112/pages/page_card_04.dart';
 //import 'package:tugas_week_8_1123150112/pages/page_card_02.dart';
-import 'package:tugas_week_8_1123150112/pages/page_card_03.dart';
+//import 'package:tugas_week_8_1123150112/pages/page_card_03.dart';
 
 void main() {
   runApp(const JullApp());
@@ -13,7 +14,7 @@ class JullApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const JulPageCard03(),
+      home: const JulPageCard04(),
     );
   }
 }
---
+++ lib/pages/page_card_04.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class JulPageCard04 extends StatelessWidget {
+  const JulPageCard04({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Page Julian Card 04"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
